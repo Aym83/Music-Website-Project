ede28c823ea93bb5e4086d6c2bf1937c1795ec6f
David <bi-crosseur@hotmail.fr>
2018-06-16 00:07:17 +0200

    Initial commit

---
+++ sheetmusic.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Music for Life - Partitions</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <!-- Load header -->
+        <?php include("header.html") ?>
+        <div class="padding border border-dark">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail border border-dark">
+                        <a href="http://media.onlinedrummer.com/wp-content/uploads/2016/07/Heathens-Drum-Sheet-Music.jpg">
+                            <img src="http://media.onlinedrummer.com/wp-content/uploads/2016/07/Heathens-Drum-Sheet-Music.jpg" alt="Heathens" style="width:100%">
+                            <div class="caption text-center">
+                                <p><b>Twenty One Pilots - Heathens</b></p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail border border-dark">
+                        <a href="http://media.onlinedrummer.com/wp-content/uploads/2016/04/Ride-Twenty-One-Pilots-Transcription.jpg">
+                            <img src="http://media.onlinedrummer.com/wp-content/uploads/2016/04/Ride-Twenty-One-Pilots-Transcription.jpg" alt="Ride" style="width:100%">
+                            <div class="caption text-center">
+                                <p><b>Twenty One Pilots - Ride </b></p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    </body>
+</html>
