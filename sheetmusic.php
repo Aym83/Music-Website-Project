3a27cd0c225bb92bef1710c7467b96d4370e9b9d
Aym83 <aym.favard@gmail.com>
2018-06-16 03:18:15 +0200

    Fix conflicts

--- sheetmusic.php
+++ sheetmusic.php
@@ -13,38 +13,14 @@ and open the template in the editor.
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
-    
     <body>
         <!-- Load header -->
         <?php include("header.html") ?>
-        <?php include("signup.html") ?>
-        <div class="padding border border-dark">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="thumbnail border border-dark">
-                        <a href="http://media.onlinedrummer.com/wp-content/uploads/2016/07/Heathens-Drum-Sheet-Music.jpg">
-                            <img src="http://media.onlinedrummer.com/wp-content/uploads/2016/07/Heathens-Drum-Sheet-Music.jpg" alt="Heathens" style="width:100%">
-                            <div class="caption text-center">
-                                <p><b>Twenty One Pilots - Heathens</b></p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail border border-dark">
-                        <a href="http://media.onlinedrummer.com/wp-content/uploads/2016/04/Ride-Twenty-One-Pilots-Transcription.jpg">
-                            <img src="http://media.onlinedrummer.com/wp-content/uploads/2016/04/Ride-Twenty-One-Pilots-Transcription.jpg" alt="Ride" style="width:100%">
-                            <div class="caption text-center">
-                                <p><b>Twenty One Pilots - Ride </b></p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div id="grid" class="padding border border-dark"></div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <script src="js/musicsheetdatabase.js"></script>
     </body>
 </html>
